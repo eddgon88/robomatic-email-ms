c7744fd8e237710e277693b952efd07cdc53c62a
edgar <eddgonzalez88@bitbucket.org>
2024-07-15 19:59:52 -0400

    fix send html template

--- application/services/mail_service.py
+++ application/services/mail_service.py
@@ -2,6 +2,8 @@ import os
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 from ..models.send_mail_request_model import SendMailRequestModel
 import logging
+from pathlib import Path
+from fastapi import HTTPException
 
 logging.basicConfig(level=logging.INFO,
                     format='(%(threadName)-10s) %(message)s',)
@@ -42,7 +44,7 @@ def get_template_message(request: SendMailRequestModel):
     return MessageSchema(
         subject=request.subject,
         recipients=request.email,
-        template_body=request.body_dict,
+        template_body=get_templete(request.template_id, request.body_dict),
         subtype=MessageType.html,
         )
 
@@ -61,3 +63,22 @@ def get_text_message(request: SendMailRequestModel):
         body=request.body,
         subtype=MessageType.plain,
         )
+
+def get_templete(template_name: str, body_dict: dict):
+    try:
+        html_file_path = Path(os.getenv('MAIL_TEMPLATE_FOLDER_DIR') + '/' + template_name)
+        if not html_file_path.exists():
+            raise HTTPException(status_code=404, detail="File html not found")
+            
+        html_content = html_file_path.read_text(encoding="utf-8")
+
+        for key, value in body_dict.items():
+            #logging.info(key)
+            html_content = html_content.replace('##'+key+'##', value)
+
+        logging.info('sending html mail - ' + html_content) 
+
+        return html_content       
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
